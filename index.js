4e6eeb1cbbd478cacfa1f8f95caa48a745660c73
@Matheus_sandes <matheus_sandes@hotmail.com>
2021-01-13 17:29:32 -0300

    finalizado

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ app.get("/", (req, res) =>{
         res.render("index")    
 });
 
+app.get("/print", (req, res) =>{
+    res.render("print")    
+});
+
+app.get("/edit", (req, res) =>{
+    res.render("edit")    
+});
+
+app.get("/table", (req, res) =>{
+    res.render("table")    
+});
+
+app.get("/teste", (req, res) =>{
+    res.render("teste01")    
+});
+
+app.get("/reserva", (req, res) =>{
+    res.render("reserva")    
+});
+
 app.listen(3000, () =>{
     console.log("app rodando");
 });
